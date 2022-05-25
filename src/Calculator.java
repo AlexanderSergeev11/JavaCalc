75f52b409247e6f081a8a57e6d0faa563ad90732
AlexanderSergeev11 <asergeevrsm@gmail.com>
2022-05-25 22:36:55 +0300

    init project

---
+++ src/Calculator.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+public class Calculator {
+    public static void main(String[] args) {
+        System.out.println("Добро пожаловать");
+        int num1 = getInt();
+        int num2 = getInt();
+        char operation = getOperation();
+        int result = calc(num1,num2,operation);
+        System.out.println("Результат: "+result);
+    }
+    public static int getInt(){
+        System.out.println("Введите число: ");
+        Scanner scanner = new Scanner(System.in);
+        int num;
+        if(scanner.hasNextLine()){
+            if (scanner.hasNextInt()) {
+                num = scanner.nextInt();
+            } else {
+                num = convert(scanner.nextLine());
+            }
+        } else {
+            System.out.println("Вы допустили ошибку. Попробуйте еще раз.");
+            scanner.next();
+            num = getInt();
+        }
+        return num;
+    }
+    public static int convert(String a) {
+        if (a.equals("I")) return 1;
+        if (a.equals("II")) return 2;
+        if (a.equals("III")) return 3;
+        if (a.equals("IV")) return 4;
+        if (a.equals("V")) return 5;
+        if (a.equals("VI")) return 6;
+        if (a.equals("VII")) return 7;
+        if (a.equals("VIII")) return 8;
+        if (a.equals("IX")) return 9;
+        if (a.equals("X")) return 10;
+        return 0;
+    }
+    public static char getOperation() {
+        System.out.println("Введите операцию:");
+        Scanner scanner = new Scanner(System.in);
+        char operation;
+        if (scanner.hasNext()) {
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
+            scanner.next();
+            operation = getOperation();
+        }
+        return operation;
+    }
+    public static int calc(int num1, int num2, char operation) {
+        int result;
+        switch (operation) {
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> result = num1 / num2;
+            default -> {
+                System.out.println("Операция не распознана. Повторите ввод.");
+                result = calc(num1, num2, getOperation());
+            }
+        }
+        return result;
+    }
+}
