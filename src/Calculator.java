abd029f62b89301a874c43872362e6ae1521e053
AlexanderSergeev11 <asergeevrsm@gmail.com>
2022-05-26 22:01:12 +0300

    add new version of calc

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,76 +1,117 @@
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.HashMap;
 
 public class Calculator {
-    static HashMap<String, Integer> romanianToInteger = new HashMap<String, Integer>();
     public static void main(String[] args) {
-        fillMap();
         System.out.println("Добро пожаловать");
-        int num1 = getInt();
-        int num2 = getInt();
-        char operation = getOperation();
-        int result = calc(num1,num2,operation);
-        System.out.println("Результат: "+result);
+        getInput();
     }
-    public static void fillMap() {
-        romanianToInteger.put("I", 1);
-        romanianToInteger.put("II", 2);
-        romanianToInteger.put("III", 3);
-        romanianToInteger.put("IV", 4);
-        romanianToInteger.put("V", 5);
-        romanianToInteger.put("VI", 6);
-        romanianToInteger.put("VII", 7);
-        romanianToInteger.put("VIII", 8);
-        romanianToInteger.put("IX", 9);
-        romanianToInteger.put("X", 10);
-    }
-    public static int getInt(){
-        System.out.println("Введите число: ");
-        Scanner scanner = new Scanner(System.in);
-        int num;
-        if(scanner.hasNextLine()){
-            if (scanner.hasNextInt()) {
-                num = scanner.nextInt();
-            } else {
-                num = convert(scanner.nextLine());
-            }
+    public static void processInput(String input) {
+        String[] arr = input.split(" ");
+        Integer int1, int2;
+        boolean isFirstNumNumeric = false, isSecondNumNumeric = false;
+        if (isNumeric(arr[0])) {
+            int1 = Integer.parseInt(arr[0]);
+            isFirstNumNumeric = true;
         } else {
-            System.out.println("Вы допустили ошибку. Попробуйте еще раз.");
-            scanner.next();
-            num = getInt();
+            int1 = convert(arr[0]);
         }
-        return num;
-    }
-    public static int convert(String a) {
-        Integer num = romanianToInteger.get(a);
-        return num;
-    }
-    public static char getOperation() {
-        System.out.println("Введите операцию:");
-        Scanner scanner = new Scanner(System.in);
-        char operation;
-        if (scanner.hasNext()) {
-            operation = scanner.next().charAt(0);
+        if (isNumeric(arr[2])) {
+            int2 = Integer.parseInt(arr[2]);
+            isSecondNumNumeric = true;
         } else {
-            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
-            scanner.next();
-            operation = getOperation();
+            int2 = convert(arr[2]);
         }
-        return operation;
-    }
-    public static int calc(int num1, int num2, char operation) {
-        int result;
-        switch (operation) {
-            case '+' -> result = num1 + num2;
-            case '-' -> result = num1 - num2;
-            case '*' -> result = num1 * num2;
-            case '/' -> result = num1 / num2;
-            default -> {
-                System.out.println("Операция не распознана. Повторите ввод.");
-                result = calc(num1, num2, getOperation());
-            }
 
+        int result = switch (arr[1]) {
+            case "*" -> int1 * int2;
+            case "+" -> int1 + int2;
+            case "/" -> int1 / int2;
+            case "-" -> int1 - int2;
+            default -> 0;
+        };
+        String romanianResult = "";
+        if (isFirstNumNumeric && !isSecondNumNumeric || !isFirstNumNumeric && isSecondNumNumeric) {
+            System.out.println("Произошла ошибка");
+            return;
         }
-        return result;
+        System.out.println("======================RESULT======================");
+        if (!isFirstNumNumeric && !isSecondNumNumeric) {
+            romanianResult = RomanNumerals(result);
+            System.out.println(romanianResult);
+        } else {
+            System.out.println(result);
+        }
+    }
+
+    public static String RomanNumerals(int Int) {
+        LinkedHashMap<String, Integer> roman_numerals = new LinkedHashMap<String, Integer>();
+        roman_numerals.put("M", 1000);
+        roman_numerals.put("CM", 900);
+        roman_numerals.put("D", 500);
+        roman_numerals.put("CD", 400);
+        roman_numerals.put("C", 100);
+        roman_numerals.put("XC", 90);
+        roman_numerals.put("L", 50);
+        roman_numerals.put("XL", 40);
+        roman_numerals.put("X", 10);
+        roman_numerals.put("IX", 9);
+        roman_numerals.put("V", 5);
+        roman_numerals.put("IV", 4);
+        roman_numerals.put("I", 1);
+        String res = "";
+        for (Map.Entry<String, Integer> entry : roman_numerals.entrySet()) {
+            int matches = Int / entry.getValue();
+            res += repeat(entry.getKey(), matches);
+            Int = Int % entry.getValue();
+        }
+        return res;
+    }
+
+    public static String repeat(String s, int n) {
+        if (s == null) {
+            return null;
+        }
+        final StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+
+    public static void getInput() {
+        System.out.println("Введите операцию: ");
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.hasNextLine()) {
+            String s = scanner.nextLine();
+            processInput(s);
+        }
+    }
+
+    public static int convert(String a) {
+        if (a.equals("I")) return 1;
+        if (a.equals("II")) return 2;
+        if (a.equals("III")) return 3;
+        if (a.equals("IV")) return 4;
+        if (a.equals("V")) return 5;
+        if (a.equals("VI")) return 6;
+        if (a.equals("VII")) return 7;
+        if (a.equals("VIII")) return 8;
+        if (a.equals("IX")) return 9;
+        if (a.equals("X")) return 10;
+        return -1;
+    }
+
+    private static boolean isNumeric(String s) {
+        if (s == null) {
+            return false;
+        }
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
     }
 }
