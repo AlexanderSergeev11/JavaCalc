605fc1ea13945adf29413f88d38e4306f1fd805c
AlexanderSergeev11 <asergeevrsm@gmail.com>
2022-05-26 21:31:31 +0300

    new version of calc

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,12 +1,28 @@
 import java.util.Scanner;
+import java.util.HashMap;
+
 public class Calculator {
+    static HashMap<String, Integer> romanianToInteger = new HashMap<String, Integer>();
     public static void main(String[] args) {
+        fillMap();
         System.out.println("Добро пожаловать");
         int num1 = getInt();
         int num2 = getInt();
         char operation = getOperation();
         int result = calc(num1,num2,operation);
         System.out.println("Результат: "+result);
+    }
+    public static void fillMap() {
+        romanianToInteger.put("I", 1);
+        romanianToInteger.put("II", 2);
+        romanianToInteger.put("III", 3);
+        romanianToInteger.put("IV", 4);
+        romanianToInteger.put("V", 5);
+        romanianToInteger.put("VI", 6);
+        romanianToInteger.put("VII", 7);
+        romanianToInteger.put("VIII", 8);
+        romanianToInteger.put("IX", 9);
+        romanianToInteger.put("X", 10);
     }
     public static int getInt(){
         System.out.println("Введите число: ");
@@ -26,17 +42,8 @@ public class Calculator {
         return num;
     }
     public static int convert(String a) {
-        if (a.equals("I")) return 1;
-        if (a.equals("II")) return 2;
-        if (a.equals("III")) return 3;
-        if (a.equals("IV")) return 4;
-        if (a.equals("V")) return 5;
-        if (a.equals("VI")) return 6;
-        if (a.equals("VII")) return 7;
-        if (a.equals("VIII")) return 8;
-        if (a.equals("IX")) return 9;
-        if (a.equals("X")) return 10;
-        return 0;
+        Integer num = romanianToInteger.get(a);
+        return num;
     }
     public static char getOperation() {
         System.out.println("Введите операцию:");
@@ -62,6 +69,7 @@ public class Calculator {
                 System.out.println("Операция не распознана. Повторите ввод.");
                 result = calc(num1, num2, getOperation());
             }
+
         }
         return result;
     }
